d015db8d00eca92ebe22e913fbc3189755b41124
Xpload <xpload@email.bg>
2017-06-27 02:27:48 +0300

    started work on forum style

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,9 @@
 const index = require('../client/routes/index');
 const users = require('../client/routes/users');
+const forum = require('../client/routes/forum');
 
 module.exports = (app) => {
     app.use('/', index);
     app.use('/users', users);
+    app.use('/forum', forum);
 };
