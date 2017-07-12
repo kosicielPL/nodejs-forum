c646d9151bd92051c740b2ec1cce3224f4bd6510
Xpload <xpload@email.bg>
2017-07-13 00:05:54 +0300

    added two unit tests WAW 'npm run coverage' to run istanbul tests

--- data/classes/forums.data.js
+++ data/classes/forums.data.js
@@ -6,6 +6,55 @@ class ForumsData extends BaseData {
         super(db, Forum, Forum);
     }
 
+    getAdminContent(count) {
+        if (count < 1 && count === 'undefined') {
+            count = 1;
+        }
+
+        let result = this.collection
+            .aggregate([{
+                    '$match': {
+                        admin: true,
+                    },
+                },
+                {
+                    '$lookup': {
+                        from: 'threads',
+                        localField: '_id',
+                        foreignField: 'forum',
+                        as: 'thread',
+                    },
+                },
+                {
+                    '$sort': {
+                        dateCreated: -1,
+                    },
+                },
+                {
+                    '$unwind': '$thread',
+                },
+                {
+                    '$sort': {
+                        'thread.dateCreated': -1,
+                    },
+                },
+                {
+                    '$limit': count,
+                },
+            ])
+            .toArray();
+
+        if (this.ModelClass.toViewModel) {
+            result = result.then((models) => {
+                return models
+                    .map((model) =>
+                        this.ModelClass.toViewModel(model));
+            });
+        }
+
+        return result;
+    }
+
     _isModelValid(model) {
         // custom validation 
         return super._isModelValid(model);
--- server/app/routers/home.router/controller.js
+++ server/app/routers/home.router/controller.js
@@ -1,9 +1,16 @@
 const init = (app, data) => {
     const controller = {
         generateHomeView(req, res) {
-            return res.render('index', {
-                title: 'Big Test Icicles',
-            });
+            data.forums
+                .getAdminContent(5)
+                .catch((error) => {})
+                .then((result) => {
+                    // res.send(result);
+                    return res.render('home', {
+                        title: 'Big Test Icicles',
+                        forums: result,
+                    });
+                });
         },
     };
 
