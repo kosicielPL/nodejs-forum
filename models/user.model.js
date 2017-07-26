1ad9396ea46964e3a5a9a84f8fdf126c0b3eac1d
Xpload <xpload@email.bg>
2017-07-26 15:07:30 +0300

    changed email regex

--- models/user.model.js
+++ models/user.model.js
@@ -2,26 +2,26 @@ const BaseModel = require('./base/base.model');
 
 class User extends BaseModel {
     static isValid(model) {
-        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const regex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
         if (typeof model === 'undefined') {
             return false;
         }
         if (typeof model.username !== 'string' &&
-            model.username.length <= 3) {
+            model.username.length < 4) {
             return false;
         }
-        if (model.password <= 3) {
+        if (model.password < 6) {
             return false;
         }
         if (!regex.test(model.email)) {
             return false;
         }
         if (typeof model.firstName !== 'string' &&
-            model.firstName.length <= 2) {
+            model.firstName.length < 3) {
             return false;
         }
         if (typeof model.firstName !== 'string' &&
-            model.firstName.length <= 2) {
+            model.firstName.length < 3) {
             return false;
         }
         return true;
