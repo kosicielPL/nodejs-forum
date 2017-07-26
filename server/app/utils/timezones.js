c39802d2c37d27f7e32c0507b15041ce651b8a9b
Xpload <xpload@email.bg>
2017-07-26 14:22:38 +0300

    added timezones users, thread view shows the date and time according to your timezone

---
+++ server/app/utils/timezones.js
@@ -0,0 +1,117 @@
+const timezones = [{
+    pretty: 'GMT -12:00',
+    zone: 'Etc/GMT+12',
+}, {
+    pretty: 'GMT -11:00',
+    zone: 'Etc/GMT+11',
+}, {
+    pretty: 'GMT -10:00',
+    zone: 'Etc/GMT+10',
+}, {
+    pretty: 'GMT -09:30',
+    zone: 'Pacific/Marquesas',
+}, {
+    pretty: 'GMT -09:00',
+    zone: 'America/Adak',
+}, {
+    pretty: 'GMT -08:00',
+    zone: 'America/Anchorage',
+}, {
+    pretty: 'GMT -07:00',
+    zone: 'America/Creston',
+}, {
+    pretty: 'GMT -06:00',
+    zone: 'America/Belize',
+}, {
+    pretty: 'GMT -05:00',
+    zone: 'America/Atikokan',
+}, {
+    pretty: 'GMT -04:00',
+    zone: 'America/Anguilla',
+}, {
+    pretty: 'GMT -03:00',
+    zone: 'America/Araguaina',
+}, {
+    pretty: 'GMT -02:30',
+    zone: 'America/St_Johns',
+}, {
+    pretty: 'GMT -02:00',
+    zone: 'America/Godthab',
+}, {
+    pretty: 'GMT -01:00',
+    zone: 'Atlantic/Cape_Verde',
+}, {
+    pretty: 'GMT +00:00',
+    zone: 'Africa/Abidjan',
+}, {
+    pretty: 'GMT +01:00',
+    zone: 'Africa/Algiers',
+}, {
+    pretty: 'GMT +02:00',
+    zone: 'Africa/Blantyre',
+}, {
+    pretty: 'GMT +03:00',
+    zone: 'Africa/Addis_Ababa',
+}, {
+    pretty: 'GMT +04:00',
+    zone: 'Asia/Baku',
+}, {
+    pretty: 'GMT +04:30',
+    zone: 'Asia/Kabul',
+}, {
+    pretty: 'GMT +05:00',
+    zone: 'Antarctica/Mawson',
+}, {
+    pretty: 'GMT +05:30',
+    zone: 'Asia/Calcutta',
+}, {
+    pretty: 'GMT +05:45',
+    zone: 'Asia/Kathmandu',
+}, {
+    pretty: 'GMT +06:00',
+    zone: 'Antarctica/Vostok',
+}, {
+    pretty: 'GMT +06:30',
+    zone: 'Asia/Rangoon',
+}, {
+    pretty: 'GMT +07:00',
+    zone: 'Antarctica/Davis',
+}, {
+    pretty: 'GMT +08:00',
+    zone: 'Asia/Brunei',
+}, {
+    pretty: 'GMT +08:30',
+    zone: 'Asia/Pyongyang',
+}, {
+    pretty: 'GMT +08:45',
+    zone: 'Australia/Eucla',
+}, {
+    pretty: 'GMT +09:00',
+    zone: 'Asia/Chita',
+}, {
+    pretty: 'GMT +09:30',
+    zone: 'Australia/Adelaide',
+}, {
+    pretty: 'GMT +10:00',
+    zone: 'Antarctica/DumontDUrville',
+}, {
+    pretty: 'GMT +10:30',
+    zone: 'Australia/LHI',
+}, {
+    pretty: 'GMT +11:00',
+    zone: 'Antarctica/Casey',
+}, {
+    pretty: 'GMT +12:00',
+    zone: 'Antarctica/McMurdo',
+}, {
+    pretty: 'GMT +12:45',
+    zone: 'NZ-CHAT',
+}, {
+    pretty: 'GMT +13:00',
+    zone: 'Etc/GMT-13',
+}, {
+    pretty: 'GMT +14:00',
+    zone: 'Etc/GMT-14',
+}];
+
+module.exports = timezones;
---
+++ server/app/utils/utils.js
@@ -0,0 +1,20 @@
+const moment = require('moment-timezone');
+const defaultTimezone = 'Europe/Sofia';
+
+const init = (timezone) => {
+    const formatter = {
+        dateFormat(format, date) {
+            return moment(date).tz(timezone).format(format);
+        },
+
+        prepareThreadContent(content){
+            return content.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '<br/>')
+        }
+    };
+
+    return formatter;
+};
+
+module.exports = {
+    init,
+};
