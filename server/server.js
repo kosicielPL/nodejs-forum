02dbfd747e2118daaef751de2c4cbc6566601141
Xpload <xpload@email.bg>
2017-07-28 11:13:23 +0300

    changes to server startup

--- server/server.js
+++ server/server.js
@@ -1,39 +1,100 @@
 /* globals process */
 const config = require('./config');
 const http = require('http');
-const port = normalizePort(process.env.PORT || config.server.port);
+const port = normalizePort(config.server.port);
 const colors = require('colors');
 
-const dbConf = config.database;
-let connectionString = 'mongodb://';
+start(config);
 
-if (dbConf.password.length > 0 && dbConf.username.length > 0) {
-    connectionString += dbConf.username + ':' + dbConf.password + '@';
+async function start(configuration) {
+    const db = await initializeDb(configuration);
+    const data = await initializeData(db);
+    const app = await initializeApp(data, config);
+
+    app.set('port', port);
+    const server = http.createServer(app);
+    require('./socketio').init(server)
+        .then((io) => {
+            app.io = io;
+        });
+    // app.io.attach(server);
+    server.listen(port, () => {
+        const startMessage = 'Listening on port: ' + port;
+        console.log(startMessage.green);
+    });
 }
 
-connectionString += dbConf.host + ':' + dbConf.port + '/' + dbConf.dbName;
+async function initializeDb(configuration) {
+    let db = null;
+    const dbConf = config.database;
+    const connectionString = createConnectionString(dbConf);
 
-console.log('Database connection: '.gray + 'mongodb://' + dbConf.host + ':'+ dbConf.port);
-console.log('Selected database: '.gray + dbConf.dbName);
+    try {
+        db = await require('../db').init(connectionString);
+    } catch (error) {
+        console.log('Database connection error!'.bgRed.white);
+        console.log(error.message);
+        process.exit(1);
+    }
 
-Promise.resolve()
-    .then(() => require('../db').init(connectionString))
-    .then((db) => require('../data').init(db))
-    .then((data) => require('./app')
-    .init(data, config.options, connectionString))
-    .then((app) => {
-        app.set('port', port);
-        const server = http.createServer(app);
-        require('./socketio').init(server)
-            .then((io) => {
-                app.io = io;
-            });
-        // app.io.attach(server);
-        server.listen(port, () => {
-            const startMessage = 'Listening on port: ' + port;
-            console.log(startMessage.green);
-        });
-    });
+    console.log('Database connection: '.gray + 'mongodb://' + dbConf.host + ':' + dbConf.port);
+    console.log('Selected database: '.gray + dbConf.dbName);
+
+    return db;
+}
+
+async function initializeData(db) {
+    let data = null;
+
+    try {
+        data = await require('../data').init(db);
+    } catch (error) {
+        console.log('Error initializing data!'.bgRed.white);
+        console.log(error);
+        process.exit(1);
+    }
+
+    return data;
+}
+
+async function initializeApp(data, configuration) {
+    let app = null;
+    const connectionString = createConnectionString(config.database);
+
+    try {
+        app =
+            await require('./app')
+            .init(data, config.options, connectionString)
+    } catch (error) {
+        console.log('Error initializing app!'.bgRed.white);
+        console.log(error);
+        process.exit(1);
+    }
+
+    return app;
+}
+
+function createConnectionString(databaseConfiguration) {
+    let connectionString = 'mongodb://';
+
+    if (databaseConfiguration.password.length > 0 &&
+        databaseConfiguration.username.length > 0) {
+        connectionString +=
+            databaseConfiguration.username +
+            ':' +
+            databaseConfiguration.password +
+            '@';
+    }
+
+    connectionString +=
+        databaseConfiguration.host +
+        ':' +
+        databaseConfiguration.port +
+        '/' +
+        databaseConfiguration.dbName;
+
+    return connectionString;
+}
 
 function normalizePort(val) {
     const portInt = parseInt(val, 10);
