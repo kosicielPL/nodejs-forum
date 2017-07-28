5b4871e719d78deea693ebc070546f6b017a2e56
Pernikoff <downingtn@abv.bg>
2017-07-28 22:29:51 +0300

    Search is done

--- server/app/routers/search.router/controller.js
+++ server/app/routers/search.router/controller.js
@@ -1,31 +1,27 @@
 const init = (app, data, config) => {
     const controller = {
+
+        showSearchForm(req, res, next) {
+            return res.render('search', {
+                title: 'Search',
+            });
+        },
+
         async showSearchResults(req, res, next) {
-            // let threadsPerPage = config.forums.forumView.threadsPerPage;
             const title = req.params.title;
+            let threadsPerPage = config.forums.forumView.threadsPerPage;
+            let page = req.params.page;
 
-            // const search = req.query.search;
+            if (page < 1 || typeof page === 'undefined') {
+                page = 1;
+            }
 
-            // if (page < 1 || typeof page === 'undefined') {
-            //     page = 1;
-            // }
-
-            // if (threadsPerPage < 1 || typeof threadsPerPage === 'undefined') {
-            //     threadsPerPage = 1;
-            // }
+            if (threadsPerPage < 1 || typeof threadsPerPage === 'undefined') {
+                threadsPerPage = 1;
+            }
 
             // let dbForum =
             //     await data.forums.getByCriteria('internalName');
-
-            // if (!dbForum || dbForum.length <= 0) {
-            //     return res.render('error', {
-            //         title: 'Error 404',
-            //         message: 'Forum not found',
-            //         error: {
-            //             status: 404,
-            //         },
-            //     });
-            // }
 
             // dbForum = dbForum[0];
 
@@ -33,36 +29,37 @@ const init = (app, data, config) => {
             //     await data.threads.getInForum(
             //         dbForum._id, threadsPerPage, page
             //     );
-
-            // if (search) {
-            //     dbThreads = dbThreads.filter((thread) => {
-            //         return thread.title.toLowerCase().includes(search);
-            //     });
-            // }
-
-            // const dbThreadsCount = dbForum.threads.length;
-
-            // let totalPages = dbThreadsCount / threadsPerPage;
-            // totalPages = Math.ceil(totalPages);
-
-            // if (page > totalPages && page > 1) {
-            //     return res.redirect(
-            //         '/forums/'
-            //     );
-            // }
-
-            // return res.render('forum/forum', {
-            //     title: dbForum.name,
-            //     forum: dbForum,
-            //     threads: dbThreads,
-            //     currentPage: page * 1,
-            //     totalPages: totalPages * 1,
-            //     threadsCount: dbThreadsCount * 1,
-            //     searchedContent: search,
-            // });
             const threads = await data.threads.findByTitle(title);
 
-            res.send(threads);
+            if (threads.length <= 0) {
+                return res.render('error', {
+                    title: 'Error 404',
+                    message: 'Nothing found about \'' + title + '\'',
+                    error: {
+                        status: 404,
+                    },
+                });
+            }
+
+            const dbThreadsCount = threads.length;
+
+            let totalPages = dbThreadsCount / threadsPerPage;
+            totalPages = Math.ceil(totalPages);
+
+            if (page > totalPages && page > 1) {
+                return res.redirect(
+                    '/forums/'
+                );
+            }
+
+            return res.render('forum/search', {
+                title: title,
+                forum: title,
+                threads: threads,
+                currentPage: page * 1,
+                totalPages: totalPages * 1,
+                threadsCount: dbThreadsCount * 1,
+            });
         },
     };
 
--- server/app/routers/search.router/router.js
+++ server/app/routers/search.router/router.js
@@ -4,15 +4,30 @@ module.exports = (app, data, config) => {
     const router = new express.Router();
     const controller = require('./controller').init(app, data, config);
 
-    router.get('/:title', (req, res, next) => {
-        if (!req.user) {
-            return Promise.resolve()
-                .then(() => {
-                    return res.redirect('/login');
+    router
+        .get('/', (req, res, next) => {
+            if (!req.user) {
+                return Promise.resolve()
+                    .then(() => {
+                        return res.redirect('/login');
                     });
             }
-        return controller.showSearchResults(req, res, next);
-    });
+            return controller.showSearchForm(req, res, next);
+        })
+        .post('/', (req, res, next) => {
+            const search = req.body.search;
+            console.log(search);
+            return res.redirect('/search/' + search);
+        })
+        .get('/:title', (req, res, next) => {
+            if (!req.user) {
+                return Promise.resolve()
+                    .then(() => {
+                        return res.redirect('/login');
+                    });
+            }
+            return controller.showSearchResults(req, res, next);
+        });
 
     return router;
 };
